c3ff46a6bd581fc35e9eefcd5be2bfbfa0a2aa40
AhmedAyoub <haddadayoubahd2003@gmail.com>
2024-04-21 23:13:16 +0100

    added the formkey and then added the validators in the text form and then checked them in the signin button

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -9,6 +9,13 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   final AuthService _auth = AuthService();
 
+  final _formKey = GlobalKey<FormState>();
+  String error = '';
+
+// text field state
+  String email = '';
+  String password = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,18 +27,43 @@ class _SignInState extends State<SignIn> {
       ),
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: ElevatedButton(
-          child: Text('sign in anon'),
-          onPressed: () async {
-            dynamic result = await _auth.signInAnon();
-            if (result == null) {
-              print('error signing in');
-            } else {
-              print('signed in');
-              print(result);
-              print(result.uid);
-            }
-          },
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 20.0),
+              TextFormField(
+                validator: (val) => val!.isEmpty ? 'Enter an email' : null,
+                onChanged: (val) {
+                  setState(() => email = val);
+                },
+              ),
+              SizedBox(height: 20.0),
+              TextFormField(
+                validator: (val) =>
+                    val!.length < 6 ? 'Enter a password 6+ chars long' : null,
+                obscureText: true,
+                onChanged: (val) {
+                  setState(() => password = val);
+                },
+              ),
+              SizedBox(height: 20.0),
+              ElevatedButton(
+                  // color: Colors.pink[400],
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.pink[400]),
+                  child: Text(
+                    'Sign In',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      print(email);
+                      print(password);
+                    }
+                  }),
+            ],
+          ),
         ),
       ),
     );
