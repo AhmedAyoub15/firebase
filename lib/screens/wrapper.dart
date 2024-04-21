3238da67be7206cb85fa64604e07506d60d8dba0
AhmedAyoub <haddadayoubahd2003@gmail.com>
2024-04-21 19:08:26 +0100

    signing annonymesly works perfectly

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,3 +1,4 @@
+import 'package:firebase/screens/authenticate/authenticate.dart';
 import 'package:firebase/screens/home/home.dart';
 import 'package:flutter/material.dart';
 
@@ -5,6 +6,6 @@ class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // return either the Home or Authenticate widget
-    return Home();
+    return Authenticate();
   }
 }
